ae7a009c9733c6cc1f576d326eed162871693a54
hanu-n <nanahsunuy@gmail.com>
2025-07-11 11:40:36 -0700

    connect to mongodb_atlas

---
+++ server.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from "./config/db.js"
+
+dotenv.config()
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  connectDB()
+});
